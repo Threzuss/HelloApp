2326f1a062214906cc1074750d567b73ab8e0f01
Threzuss <hk6437@srmist.edu.in>
2026-03-10 09:00:22 +0530

    Implemented UC1

---
+++ HelloApp.java
@@ -0,0 +1,17 @@
+/**
+ * HelloApp.java - A simple Java application that displays "Hello, World!"
+ * * This demonstrates fundamental Java concepts such as class declaration, 
+ * the main method, and standard output.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class HelloApp {
+    
+    // The main method serves as the entry point for program execution
+    public static void main(String[] args) {
+        
+        // Prints the specified message followed by a newline character
+        System.out.println("Hello, World!");
+    }
+}
